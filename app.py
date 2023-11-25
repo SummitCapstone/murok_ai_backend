b4b810993b251db06e464967a0c9331e105e15b9
LeeDayday <dayoon0905@gmail.com>
2023-11-26 02:23:16 +0900

    chore: Remove unnecessary response for GET requests in diagnosis route

--- app.py
+++ app.py
@@ -3,14 +3,9 @@ from diagnosis import diagnose
 app = Flask(__name__)
 
 
-@app.route('/diagnosis', methods=['GET', 'POST'])
+@app.route('/diagnosis', methods=['POST'])
 def diagnosis():
     image = request.files['image']
     crop_type = request.form['crop_type']
-    # Handle both GET and POST requests here
-    if request.method == 'GET':
-        return {"crop_type": crop_type}
-    # Assuming you want to handle POST requests as well
-
     top_diseases, top_probabilities = diagnose(image, crop_type)
     return {'top_diseases': top_diseases, 'top_probabilities': top_probabilities}
