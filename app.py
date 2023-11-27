fa38450edeecbaded500ac1663ee4de63b1b62a5
Ubuntu <ubuntu@ip-172-31-29-195.ap-southeast-2.compute.internal>
2023-11-27 02:53:58 +0000

    chore: Added logging functionality

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
+import logging
 from flask import Flask, request
 from diagnosis import diagnose
+
 app = Flask(__name__)
 
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 @app.route('/diagnosis', methods=['POST'])
 def diagnosis():
-    image = request.files['image']
-    crop_type = request.form['crop_type']
-    top_diseases, top_probabilities = diagnose(image, crop_type)
-    return {'top_diseases': top_diseases, 'top_probabilities': top_probabilities}
+    try:
+        image = request.files['image']
+        crop_type = request.form['crop_type']
+        
+        logging.debug(f"Received POST Request for crop_type={crop_type}")
+
+        top_diseases, top_probabilities = diagnose(image, crop_type)
+
+        logging.debug("Diagnosis completed successfully")
+
+        return {'top_diseases': top_diseases, 'top_probabilities': top_probabilities}
+
+    except Exception as e:
+        logging.error(f"Error during diagnosis: {str(e)}")
+        return {'error': str(e)}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
--- diagnosis.py
+++ diagnosis.py
@@ -4,11 +4,40 @@ from PIL import Image
 from utils import *
 import timm
 import requests
+import logging
+import time
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 N_classes = 9
 
+logger.debug(f"device: {device}")
+
+def create_model(crop_type):
+  logger.debug(f"Creating {crop_type} model...")
+  model_type = get_model_by_crop(crop_type)
+  logger.debug("Received model_type...")
+  model = timm.create_model(model_type, pretrained=False, num_classes=N_classes)
+  logger.debug("Received model...")
+  WEIGHT_PATH = get_weight_by_crop(crop_type)
+  logger.debug("Received weight path...")
+  # 모델 가중치 로드
+  model.load_state_dict(torch.load(WEIGHT_PATH, map_location=device))
+  logger.debug("Loaded Model...")  
+  model.eval()
+  logger.debug("Model created and loaded successfully.")
+  return model
+# Load model in advance
+starwberry_model = create_model('strawberry')
+cucumber_model = create_model('cucumber')
+tomato_model = create_model('tomato')
+pepper_model = create_model('pepper')
+
+
 def load_and_preprocess_image(image_path):
+  logger.debug("Loading and preprocessing image...")  
   # 이미지 로드 및 전처리
   image = Image.open(image_path).convert("RGB")
   preprocess = transforms.Compose([
@@ -17,33 +46,32 @@ def load_and_preprocess_image(image_path):
   ])
   image = preprocess(image)
   image = image.unsqueeze(0)  # 배치 차원 추가
+  logger.debug("Image loaded and preprocessed successfully.")
   return image
 
 
-def create_model(crop_type):
-  model_type = get_model_by_crop(crop_type)
-  model = timm.create_model(model_type, pretrained=False, num_classes=N_classes)
-  WEIGHT_PATH = get_weight_by_crop(crop_type)
-
-  # 모델 가중치 저장
-  torch.save(model.state_dict(), WEIGHT_PATH)
-
-  # 모델 가중치 로드
-  model.load_state_dict(torch.load(WEIGHT_PATH, map_location=device))
-  model.eval()
-
-  return model
 
 
 def diagnose(image_path, crop_type):
+  logger.info("Starting diagnosis...")
+  start_time = time.time()
   # 이미지 로드 및 전처리
   image = load_and_preprocess_image(image_path)
 
   # 모델 호출
-  model = create_model(crop_type)
-
-  # 추론
-  with torch.no_grad():
+  if crop_type == 'strawberry':
+      model = strawberry_model
+  elif crop_type == 'cucumber':
+      model = cucumber_model
+  elif crop_type == 'tomato':
+      model = tomato_model
+  elif crop_type == 'pepper':
+      model = pepper_model
+  else:
+      raise ValueError("Invalid crop_type")
+      
+  # 추론 
+  with  torch.no_grad():
     outputs = model(image)
   # 상위 3개의 클래스 및 확률 추출
   _, top_classes = torch.topk(outputs, 3, dim=1)
@@ -60,5 +88,7 @@ def diagnose(image_path, crop_type):
   softmax = torch.nn.Softmax(dim=1)
   probabilities = softmax(outputs)[0].tolist()
   top_probabilities = [probabilities[class_label] for class_label in top_classes]
-
+  end_time = time.time()
+  elapsed_time = end_time - start_time
+  logger.info(f"Diagnosis completed in {elapsed_time} seconds.")
   return top_diseases, top_probabilities
