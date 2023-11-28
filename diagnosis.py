3901342a1c336df90e4087a41d6ce403cb54692b
Ubuntu <ubuntu@ip-172-31-29-195.ap-southeast-2.compute.internal>
2023-11-28 07:22:01 +0000

    feat: Translated PyTorch-based diagnosis code to TensorFlow

--- diagnosis.py
+++ diagnosis.py
@@ -1,9 +1,10 @@
-import torch
-from torchvision import transforms
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.optimizers import AdamW
 from PIL import Image
-from utils import *
-import timm
-import requests
+import numpy as np
+from  utils import *
+from vit_structure import *
 import logging
 import time
 import os
@@ -11,42 +12,35 @@ import os
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda' if tf.config.list_physical_devices('GPU') else 'cpu'
 N_classes = 9
 
 logger.debug(f"device: {device}")
 
 # s3에서 파일 불러오기
 
-if len(os.listdir('/home/ubuntu/murok_ai_backend/weights/')) != 4:
+if len(os.listdir('/home/ubuntu/murok_ai_backend/weights/')) < 4:
     classes = ['strawberry', 'cucumber', 'tomato', 'pepper']
     for c in classes:
         download_weights(c)
-    logger.debug(f"Downloaded weight files from S3")
+        logger.debug(f"Downloaded {c} weight file from S3")
 else:
     logger.debug("Weight files already downloaded")
 
 
 def create_model(crop_type):
-    logger.debug(f"Creating {crop_type} model...")
-    model_type = 'vit_base_patch16_224' 
-    logger.debug("Received model_type...")
-    # model = timm.create_model(model_type, pretrained=False, num_classes=N_classes)
-    logger.debug("Received model...")
-    WEIGHT_PATH = get_weight_by_crop(crop_type)
-    logger.debug("Received weight path...")
-    # 모델 가중치 로드
-    model = tf.keras.models.load_model(WEIGHT_PATH)
-    logger.debug("Loaded Model...")  
-    model.eval()
-    logger.debug("Model created and loaded successfully.")
+    h5_file = get_weight_by_crop(crop_type)
+    logger.debug(f"h5_file: {h5_file}")
+    model = tf.keras.models.load_model(h5_file)
+    logger.debug(f"Model loaded")
     return model
 
 # 모델 가중치 미리 불러오기
-starwberry_model = create_model('strawberry')
-cucumber_model = create_model('cucumber')
-tomato_model = create_model('tomato')
-pepper_model = create_model('pepper')
+with tf.keras.utils.custom_object_scope({'Patches': Patches, 'PatchEncoder': PatchEncoder, 'AdamW': AdamW}):
+    strawberry_model = load_model(get_weight_by_crop('strawberry'), compile=False)
+    cucumber_model = load_model(get_weight_by_crop('cucumber'), compile=False)
+    tomato_model = load_model(get_weight_by_crop('tomato'), compile=False)
+    pepper_model = load_model(get_weight_by_crop('pepper'), compile=False)
 
 
 def get_model_by_crop(crop_type):
@@ -67,12 +61,12 @@ def load_and_preprocess_image(image_path):
     logger.debug("Loading and preprocessing image...")  
     # 이미지 로드 및 전처리
     image = Image.open(image_path).convert("RGB")
-    preprocess = transforms.Compose([
-      transforms.Resize((224, 224)),
-      transforms.ToTensor(),
-    ])
-    image = preprocess(image)
-    image = image.unsqueeze(0)  # 배치 차원 추가
+    image = tf.convert_to_tensor(np.array(image))
+    image = tf.image.resize(image, [128, 128])
+    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+
+    # 배치 차원 추가
+    image = tf.expand_dims(image, 0)
     logger.debug("Image loaded and preprocessed successfully.")
     return image
 
@@ -84,24 +78,32 @@ def diagnose(image_path, crop_type):
     image = load_and_preprocess_image(image_path)
     # 모델 호출
     model = get_model_by_crop(crop_type) 
-    # 추론 
-    with  torch.no_grad():
-      outputs = model(image)
+    logger.debug(f"Get {crop_type} model")
+# 추론 
+    with tf.GradientTape(persistent=True) as tape:
+        tape.watch(image)
+        outputs = model(image)
+        outputs = tf.stop_gradient(outputs)
+    logger.debug(f"Completed Inference")
     # 상위 3개의 클래스 및 확률 추출
-    _, top_classes = torch.topk(outputs, 3, dim=1)
-    top_classes = top_classes.squeeze().tolist()
+    top_classes = tf.math.top_k(outputs, k=3).indices.numpy()[0].tolist()
+    
     # 클래스를 병명으로 변환
     disease_map = get_disease_map()
     label_map = get_label_map(crop_type)
+    
     # 병명 코드 구하기
     code_list = [label_map[i] for i in top_classes]
+    
     # 병명 코드로부터 병명 구하기
     top_diseases = [disease_map[code] for code in code_list]
-
+    logger.debug(f"top diseases: {top_diseases}")
     # 각 클래스의 확률값 추출
-    softmax = torch.nn.Softmax(dim=1)
-    probabilities = softmax(outputs)[0].tolist()
+    softmax = tf.keras.layers.Softmax(axis=1)
+    probabilities = softmax(outputs)[0].numpy()
     top_probabilities = [probabilities[class_label] for class_label in top_classes]
+    top_probabilities = [float(probability) for probability in top_probabilities]
+
     end_time = time.time()
     elapsed_time = end_time - start_time
     logger.info(f"Diagnosis completed in {elapsed_time} seconds.")
